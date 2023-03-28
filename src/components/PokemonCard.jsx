fc5b1f0c625481c41a451ad15008bbf6b07f37c9
JeanBat0202 <jeanbaptiste.blond@gmail.com>
2023-03-28 16:16:50 +0200

    exo React Basics 03

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,33 @@
 function PokemonCard(){
+    const pokemon = pokemonList[0];
+    if (pokemon.imgSrc ){
     return(
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"/>
+            <img src={pokemon.imgSrc} alt={pokemon.name}/>
+            <figcaption>'{pokemon.name}'</figcaption>
         </figure>
-    );
+    )
+    } else {
+      return(
+        <figure>
+        <p>???</p>
+        <figcaption>{pokemon.name}</figcaption>
+    </figure>
+      )
+    }
 }
+
+
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCard;
