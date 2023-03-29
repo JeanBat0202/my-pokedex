94ca5cb5eb25cf35606ddfac8810c26d24be65f1
JeanBat0202 <jeanbaptiste.blond@gmail.com>
2023-03-29 12:00:11 +0200

    props.1

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,35 @@ import './App.css'
 import PokemonCard from './components/PokemonCard.jsx'
 
 
+
 function App(){
   return(
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]}/>
     </div>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
+
+// function App(props){
+//   return(
+//     <div>
+//       <props.PokemonCard/>
+//     </div>
+//   );
+// }
+
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,8 @@
-function PokemonCard(){
-    const pokemon = pokemonList[0];
+import App from "../App.jsx";
+
+
+function PokemonCard({pokemon}){
+    
     if (pokemon.imgSrc ){
     return(
         <figure>
@@ -17,17 +20,5 @@ function PokemonCard(){
     }
 }
 
-
-
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
 export default PokemonCard;
